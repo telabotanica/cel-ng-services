e18b55a13b17f66b622032b176d55d50ebd9a2ca
slack <yogi.margarine@yahoo.com>
2019-04-16 21:05:42 +0200

    Used tmp folder from .env files + code beautification.

--- src/Serializer/PdfOccurrenceEncoder.php
+++ src/Serializer/PdfOccurrenceEncoder.php
@@ -2,27 +2,25 @@
 
 namespace App\Serializer;
 
+use App\Serializer\OccurrencePdfGenerator;
+
 use Symfony\Component\Serializer\Encoder\EncoderInterface;
 use Symfony\Component\Serializer\Encoder\DecoderInterface;
-use App\Serializer\OccurrencePdfGenerator;
+use Symfony\Component\Dotenv\Dotenv;
 
 /**
  * Encodes/returns a PDF file representation of a set of Occurence instances.
  */
-class PdfOccurrenceEncoder implements EncoderInterface, DecoderInterface
-{
+class PdfOccurrenceEncoder implements EncoderInterface, DecoderInterface {
 
     /**
      * @inheritdoc
      */
-    public function encode($data, $format, array $context = array())
-    {
+    public function encode($data, $format, array $context = array()) {
         $pdfGenerator = new OccurrencePdfGenerator();
         $pdf = $pdfGenerator->export($data);
-
         $now = date_format(new \DateTime('now'), 'd_m_Y_H_i_s');
-      
-        $filename = '/tmp/' . $now . '.pdf';
+        $filename = getenv('TMP_FOLDER') . $now . '.pdf';
         $pdfGenerator->pdf->Output($filename,'F');
 
         return file_get_contents($filename);
@@ -31,25 +29,23 @@ class PdfOccurrenceEncoder implements EncoderInterface, DecoderInterface
     /**
      * @inheritdoc
      */
-    public function supportsEncoding($format)
-    {
+    public function supportsEncoding($format) {
         return 'pdf' === $format;
     }
 
     /**
      * @inheritdoc
      */
-    public function decode($data, $format, array $context = array())
-    {
+    public function decode($data, $format, array $context = array()) {
         return null;
     }
 
     /**
      * @inheritdoc
      */
-    public function supportsDecoding($format)
-    {
+    public function supportsDecoding($format) {
         // No Web service consumes/decodes PDF files. Some only produce some.
         return false;
     }
+
 }
