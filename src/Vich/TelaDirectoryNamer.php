0aa499f20636e1939fb2171b47b4c42cb55d2940
slack <yogi.margarine@yahoo.com>
2019-07-09 06:35:12 +0200

    BF: vich namers - fixed extra path separators + a bit of refactoring.

--- src/Vich/TelaDirectoryNamer.php
+++ src/Vich/TelaDirectoryNamer.php
@@ -6,26 +6,24 @@ use Vich\UploaderBundle\Naming\DirectoryNamerInterface;
 use Vich\UploaderBundle\Mapping\PropertyMapping;
 
 /**
- * Namer class.
+ * Namer class. Called during the prePersist/preUpdate doctrine events.
  */
 class TelaDirectoryNamer implements DirectoryNamerInterface {
 
     /**
      * {@inheritdoc}
      */
-    public function directoryName($obj, PropertyMapping $mapping): string {
+    public function directoryName($object, PropertyMapping $mapping): string {
 
-        if ( null !== $obj->getId() ) {
-            return TelaDirectoryNamer::buildTelaPhotoApiFolder($obj);
-        }
-        else {
-            return getEnv("TMP_FOLDER");
-        }
+
+        return ( null !== $object->getId() ) ? 
+            TelaDirectoryNamer::buildTelaPhotoApiFolderName($object) : 
+            getEnv("TMP_FOLDER") . '/';
     }
 
-    public static function buildTelaPhotoApiFolder($entity) {
+    public static function buildTelaPhotoApiFolderName($entity): string {
         $obsStrId = str_pad(strval($entity->getId()), 9, "0", STR_PAD_LEFT);
-        return getEnv('BASE_TELA_PHOTO_API_DIR') . \DIRECTORY_SEPARATOR . substr($obsStrId, 0, 3) . \DIRECTORY_SEPARATOR . substr($obsStrId, 3, 3) .  \DIRECTORY_SEPARATOR . 'O' . \DIRECTORY_SEPARATOR ;
+        return getEnv('BASE_TELA_PHOTO_API_DIR') . substr($obsStrId, 0, 3) . \DIRECTORY_SEPARATOR . substr($obsStrId, 3, 3) .  \DIRECTORY_SEPARATOR . 'O'  ;
     }
 
 
