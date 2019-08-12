9563e0ab6ceb45ccb29fda360bc951e034ac4423
slack <yogi.margarine@yahoo.com>
2019-08-12 13:45:07 +0200

    Forgot to add getters and setters to ChangeLog entity

--- src/Entity/ChangeLog.php
+++ src/Entity/ChangeLog.php
@@ -27,7 +27,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\OccurrenceRepository")
+ * @ORM\Entity()
  * @ORM\Table(name="change_log")
  */
 class ChangeLog  {
@@ -47,7 +47,6 @@ class ChangeLog  {
      */
     private $entityId = null;
 
-
    /**
      * Type of action to be mirrored in the ES index.
      *
@@ -61,5 +60,36 @@ class ChangeLog  {
      * @ORM\Column(name="entity_name", type="string", nullable=false, options={"comment":"Nom de l'entité sur laquelle porte l'action à répercuter."})
      */
     private $entityName = null;
+
+
+    public function getEntityName(): string {
+        return $this->entityName;
+    }
+
+    public function setEntityName(string $entityName): self {
+        $this->entityName = $entityName;
+
+        return $this;
+    }
+
+    public function getEntityId(): int {
+        return $this->entityId;
+    }
+
+    public function setEntityId(int $entityId): self {
+        $this->entityId = $entityId;
+
+        return $this;
+    }
+
+    public function getActionType(): string {
+        return $this->actionType;
+    }
+
+    public function setActionType(string $actionType): self {
+        $this->actionType = $actionType;
+
+        return $this;
+    }
 
 }
