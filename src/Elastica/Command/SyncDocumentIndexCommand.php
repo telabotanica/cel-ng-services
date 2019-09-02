aa3ba75995bd6d18c0c18d40a8f5f66ae483ed1c
slack <yogi.margarine@yahoo.com>
2019-09-02 17:21:40 +0200

    Using elastica persister instead of Doctrine hack to update ES documents.

--- src/Elastica/Command/SyncDocumentIndexCommand.php
+++ src/Elastica/Command/SyncDocumentIndexCommand.php
@@ -7,6 +7,7 @@ use App\Elastica\Command\UnknownEntityNameException;
 
 use FOS\ElasticaBundle\Persister\ObjectPersisterInterface;
 
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -14,9 +15,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-
-
-use FOS\ElasticaBundle\Index\IndexManager;
+use FOS\ElasticaBundle\Persister\ObjectPersister;
 
 /** 
  * Simple command which loads change notifications from change_log table and 
@@ -27,15 +26,18 @@ class SyncDocumentIndexCommand  extends Command {
 
     private $changeLogs;
     private $entityManager;
+    private $occurrencePersister;
+    private $photoPersister;
     private const ALLOWED_ENTITY_NAMES = ['occurrence', 'photo'];
 
 
     public function __construct(ContainerInterface $container) {
         $this->entityManager = $container->get('doctrine')->getManager();
+        $this->occurrencePersister = $container->get('fos_elastica.object_persister.occurrences.occurrence');
+        $this->photoPersister = $container->get('fos_elastica.object_persister.photos.photo');
         parent::__construct();
     }
 
-    
     protected function configure() {
         $this
             ->setName('cel:sync-es')
@@ -81,13 +83,13 @@ class SyncDocumentIndexCommand  extends Command {
             case "create":
                 $entity = $this->getRepository($changeLog->getEntityName())->find($changeLog->getEntityId());
                 if ($entity !== null) {
-                    $this->createDocument($entity);
+                    $this->createDocument($entity, $changeLog->getEntityName());
                 }
             break;
             case "update":
                 $entity = $this->getRepository($changeLog->getEntityName())->find($changeLog->getEntityId());
                 if ($entity !== null) {
-                    $this->updateDocument($entity);
+                    $this->updateDocument($entity, $changeLog->getEntityName());
                 }
             break;
             case "delete":
@@ -104,21 +106,24 @@ class SyncDocumentIndexCommand  extends Command {
         return $this->entityManager->getRepository('App:' . ucfirst($entityClassName));
     }
 
-
+    private function getPersister($entityClassName) {
+        if ( $entityClassName == 'occurrence') {
+            return $this->occurrencePersister;
+        }
+        return $this->photoPersister;
+    }
 
     private function deleteDocument(int $id, string $resourceTypeName) {
         ElasticsearchClient::deleteById($id, $resourceTypeName);
     }
 
-    private function updateDocument(object $entity) {
-        // Just loads the entity, updates its new dateUpdated and  persists it. It will trigger index insertion/update
-        $entity->setDateUpdated(new \DateTime());
-        $this->entityManager->flush();        
+    private function updateDocument(object $entity, $entityName) {
+        $this->getPersister($entityName)->replaceOne($entity);
     }
 
 
-    private function createDocument(object $entity) {
-        $this->updateDocument($entity);
+    private function createDocument(object $entity, $entityName) {
+        $this->getPersister($entityName)->replaceOne($entity);
     }
 
 }   
