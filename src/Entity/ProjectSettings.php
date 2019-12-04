483ecd2beb6b9f477f1439e623679d231af00b1e
slack_lpm <s.renaudier@gmail.com>
2019-12-04 10:16:15 +0100

    Fix typo according to definitive model

--- src/Entity/ProjectSettings.php
+++ src/Entity/ProjectSettings.php
@@ -156,7 +156,7 @@ class ProjectSettings
     * Valeur(s) par défaut du champ 'environment' (milieux) de toutes les obs du projet
     *
     * @Groups({"read"})
-    * @ORM\Column(name="environments", type="string", nullable=true, options={"comment":"Valeur(s) par défaut du champ 'environment' (milieux) de toutes les obs du projet"})
+    * @ORM\Column(name="environment", type="string", nullable=true, options={"comment":"Valeur(s) par défaut du champ 'environment' (milieux) de toutes les obs du projet"})
     */
    private $environment = null;
 
